c610f751556dd32bb7708bc11dffaeb937df3cbd
mbertero <macabertero@gmail.com>
2023-04-20 21:03:46 -0400

    Variables desde js

--- ClaseForm/js/app.js
+++ ClaseForm/js/app.js
@@ -5,3 +5,10 @@ document.write("<h1>Mensaje de alerta de js<h1/>")
 
 //  Mensaje de alerta
 //alert("danger....");
+
+var nt1 = 4.5;
+var nt2 = 2.0;
+var nt3 = 7.0;
+
+var suma = nt1 + nt2+nt3;
+console.log("La suma de sus notas es: ", suma);
